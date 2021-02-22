9494343f37b6aec8711e5b9f6ef205e040ad1b6f
Matthew Ouyang <wouy448@aucklanduni.ac.nz>
2021-02-22 23:18:36 +1300

    fix cors error

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const cors = require("cors");
+app.use(cors());
+
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
   console.log(`Running on port: ${PORT}`);
